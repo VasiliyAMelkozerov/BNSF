47bc7325baf0e30e8de60fc69f6918d876a28193
VasiliyAMelkozerov <vasiliy.melkozerov@gmail.com>
2023-02-08 20:14:31 -0600

    Turnin

--- tester.py
+++ tester.py
@@ -1,2 +1,17 @@
-'''Whtat seemds to be the test here that I shall deploy
+'''What seemds to be the test here that I shall deploy
 Well for one getting in on the a comment and saved file would be a good way to go'''
+
+"""
+    So in looking into what I can possibly do with this project I am able to
+    ...
+    still tryna find out
+    what I can do defintily 
+    is find out how to look at and read a quarterly earnings report
+    from here I can identify what type of data I will be sifting through 
+    and the two types of data here is categorical and numerical
+    all data boils down into this
+    I can say whether something was a success or not
+    Okay but the clearest/closest next step is to look up how to read 
+"""
+
+"the first takeaway here is that it has to be compared to previous reports as to be our reference "
